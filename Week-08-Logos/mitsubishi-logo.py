a6248e840f1a78d5aef131f7b485d5ca744daf9d
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2021-02-20 14:42:55 +0500

    Added comments

--- Week-08-Logos/mitsubishi-logo.py
+++ Week-08-Logos/mitsubishi-logo.py
@@ -5,13 +5,14 @@ myTurtle.shape('turtle')
 myTurtle.speed(3)
 myTurtle.pensize(2)
 
-
+# function to draw a single leaf
 def draw_leaf(length):
     for i in range(4):
         angle= 60 if i%2==0 else 120 
         myTurtle.forward(length)
         myTurtle.right(angle)
 
+# function to iteratively draw leafs for the complete logo
 def draw_pattern(length):
     for i in range(3):
         draw_leaf(length)
--- Week-08-Logos/sierpinski-triangle.py
+++ Week-08-Logos/sierpinski-triangle.py
@@ -6,22 +6,31 @@ myTurtle.shape('turtle')
 myTurtle.pensize(2)
 myTurtle.speed(100)
 
+# Draws a single triangle 
 def draw_triangle(length):
     for i in range(3):
         myTurtle.forward(length)
         myTurtle.left(120)
 
+# Draws the sierpinski triangle by recursively calling itself
 def draw_sierpinski(length,depth):
+    # Base case
     if depth==0:
         draw_triangle(length)
     else:
+        # First child triangle 
         draw_sierpinski(length=length/2,depth=depth-1)
+        # Get turtle to starting position for 1st triangle
         myTurtle.forward(length/2)
+        # Second child triangle
         draw_sierpinski(length=length/2,depth=depth-1)
+        # Get turtle to starting position for 2nd triangle
         myTurtle.left(120)
         myTurtle.forward(length/2)
         myTurtle.left(-120)
+        # Third child triangle
         draw_sierpinski(length=length/2,depth=depth-1)
+        # Return the turtle back to the orginal starting position and angle.
         myTurtle.left(60)
         myTurtle.back(length/2)
         myTurtle.left(-60)
